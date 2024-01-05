8b00414ab5ab62787ca9b88f3ca39dca0f3ba067
ketyung <ketyung@techchee.com>
2024-01-05 12:11:27 +0800

    Initial Commit

---
+++ SwiftUIImagePicker/SwiftUIImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImagePickerApp.swift
+//  SwiftUIImagePicker
+//
+//  Created by Christopher Chee on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
